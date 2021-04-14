afe6078b2d6c40a87211c7ed395afc6bec2c43c8
Jonatas Daniel Hermann <jonatas.hermann@gmail.com>
2021-04-14 09:58:29 -0300

    Change plan amount to float

--- lib/stripe_mock/request_handlers/invoices.rb
+++ lib/stripe_mock/request_handlers/invoices.rb
@@ -99,7 +99,7 @@ module StripeMock
         invoice_lines = []
 
         if prorating
-          unused_amount = subscription[:plan][:amount] * subscription[:quantity] * (subscription[:current_period_end] - subscription_proration_date.to_i) / (subscription[:current_period_end] - subscription[:current_period_start])
+          unused_amount = subscription[:plan][:amount].to_f * subscription[:quantity] * (subscription[:current_period_end] - subscription_proration_date.to_i) / (subscription[:current_period_end] - subscription[:current_period_start])
           invoice_lines << Data.mock_line_item(
                                    id: new_id('ii'),
                                    amount: -unused_amount,
